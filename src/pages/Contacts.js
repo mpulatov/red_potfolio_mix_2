43e5ce389832e06d408475878cb25f5265c1c1a4
Muhammad Pulatov <mpulatov.developer@gmail.com>
2023-03-04 16:24:43 +0500

    (_feat): Portfolio for mix - 2

---
+++ src/pages/Contacts.js
@@ -0,0 +1,30 @@
+const Contacts = () => {
+  return (
+    <main className="section">
+      <div className="container">
+        <h1 className="title-1">Contacts</h1>
+
+        <ul className="content-list">
+          <li className="content-list__item">
+            <h2 className="title-2">Location</h2>
+            <p>Dushanbe, Tajikistan</p>
+          </li>
+          <li className="content-list__item">
+            <h2 className="title-2">Telegram / WhatsApp</h2>
+            <p>
+              <a href="tel:+992800008016">+922 (800) 00-80-16</a>
+            </p>
+          </li>
+          <li className="content-list__item">
+            <h2 className="title-2">Email</h2>
+            <p>
+              <a href="mailto:webdev@protonmail.com">mpulatov1995@gmail.com</a>
+            </p>
+          </li>
+        </ul>
+      </div>
+    </main>
+  );
+};
+
+export default Contacts;
